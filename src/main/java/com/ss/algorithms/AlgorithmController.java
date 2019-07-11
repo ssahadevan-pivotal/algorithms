ada5b3da34a22c3d4e54d553be61301639564c57
Sharath Sahadevan <ssahadevan@pivotal.io>
2019-07-11 16:18:49 -0500

    Added Thymeleaf
    Upddated controller to point to viewResult.html and viewUsage.html

--- src/main/java/com/ss/algorithms/AlgorithmController.java
+++ src/main/java/com/ss/algorithms/AlgorithmController.java
@@ -18,7 +18,8 @@ import org.springframework.web.client.RestTemplate;
  * @date:   March 2019
  */
 
-@RestController
+// @RestController
+@Controller
 public class AlgorithmController {
 	
 	  //private CredhubProperties credhubProperties;
@@ -29,28 +30,31 @@ public class AlgorithmController {
 	  String usage="Usage: http://localhost:8080/arithmetic?firstValue=15&secondValue=5&operator=add" ;
 	
 	   @GetMapping("/")
-	   public String getIndex() {
-		   return "index.html" ;
+	   public String getIndex(Model model) {
+		   return "index" ;
 	   }
 	   
 	   @GetMapping("/arithmetic")
-	    public String getArithmetic(@RequestParam(value="firstValue", defaultValue="10") String firstValue 
+	    public String getArithmetic(Model model , @RequestParam(value="firstValue", defaultValue="10") String firstValue 
 	    		             , @RequestParam(value="secondValue", defaultValue="10") String secondValue
 	    		             , @RequestParam(value="operator", defaultValue="add") String operator
 	    		)  {
 	    	int firstVal = Integer.parseInt( firstValue ) ;
 	    	int secondVal = Integer.parseInt( secondValue ) ;
 	    	// String usage="Usage: http://localhost:8080/arithmetic?firstValue=15&secondValue=5&operator=add" ;
+	    	model.addAttribute("Usage", usage);
 	    	Arithmetic arithmeticClass;
 			try {
 				arithmeticClass = new Arithmetic( firstVal , secondVal, operator);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				return( usage);
+				return( "viewUsage");
 			}
 			System.out.println( "Result is " + arithmeticClass.getResult());
-			return (  "Result is " + arithmeticClass.getResult() );
+			model.addAttribute("Result",  "Result is " + arithmeticClass.getResult());
+			//return (  "Result is " + arithmeticClass.getResult() );
+			return ( "viewResult" );
 			
 	    	//return ("5") ;
 	    	
@@ -59,7 +63,7 @@ public class AlgorithmController {
 	       
 	    // Greatest Common Divisor  - Test
 	    @GetMapping("/gcd")
-	    public String gcd( 
+	    public String gcd( Model model ,
 	    		         @RequestParam(value="firstValue", defaultValue="10") String firstValue 
 	                     ,@RequestParam(value="secondValue", defaultValue="10") String secondValue
 	    		) {
@@ -67,11 +71,12 @@ public class AlgorithmController {
 	    	int secondVal = Integer.parseInt( secondValue ) ;
 	    	GreatestCommonDivisor gcd = new GreatestCommonDivisor( firstVal, secondVal);
 			System.out.println( "Result is " + gcd.getResult());
-			return (  "Result is " + gcd.getResult() );
+			model.addAttribute("Result",  "Result is " + gcd.getResult() );
+			return ( "viewResult" );
 	    }
 	    
 	    @GetMapping("/fibonacciseries")
-	    public String getfibonacciseries(@RequestParam(value="resultCount", defaultValue="5") String resultCount ) 
+	    public String getfibonacciseries(Model model ,@RequestParam(value="resultCount", defaultValue="5") String resultCount ) 
 	    {	             
 	    	int count = Integer.parseInt( resultCount ) ;
 	    	//int secondVal = Integer.parseInt( secondValue ) ;
@@ -82,8 +87,8 @@ public class AlgorithmController {
 	    	int secondVal = 1;
 	    	String operator="add";
 	    	
-	    	
-	    	while ( count > 0 )
+	    	model.addAttribute("Usage", usage);
+	    	while ( count > 2 )
 	    	{
 	    		try {
 					arithmetic= new Arithmetic( firstVal, secondVal , operator );
@@ -94,18 +99,19 @@ public class AlgorithmController {
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-					return usage;
+					return ("viewUsage") ;
 				}
 	    	}
 	    	
 			//System.out.println( "Result is " + arithmeticClass.getResult());
-			return (  "Fibonacci series for " + resultCount + " numbers is " + result );
+	    	model.addAttribute("Result",  "Fibonacci series for " + resultCount + " numbers is " + result);
+			return ( "viewResult");   //View is result.html
 			
 	    	
 	    }
 	    
 	    @GetMapping("/geometricseries")
-	    public String getgeometricseries(
+	    public String getgeometricseries( Model model ,
 	    		 @RequestParam(value="firstValue", defaultValue="3") String firstValue 
                  ,@RequestParam(value="secondValue", defaultValue="2") String secondValue
 	    		 , @RequestParam(value="resultCount", defaultValue="5") String resultCount 
@@ -122,7 +128,7 @@ public class AlgorithmController {
 	    	Arithmetic arithmetic = null ;
 	    	String operator="multiply";
 	    	
-	    	
+	    	model.addAttribute("Usage", usage);
 	    	while ( count > 0 )
 	    	{
 	    		try {
@@ -134,19 +140,20 @@ public class AlgorithmController {
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-					return usage;
+					return ("viewUsage") ;
 				}
 	    	}
 	    	
 			//System.out.println( "Result is " + arithmeticClass.getResult());
-			return (  "Geometric series for " + resultCount + " numbers is " + result );
+	    	model.addAttribute("Result", ( "Geometric series for " + resultCount + " numbers is " + result));
+			return (  "viewResult" );
 			
 	    	
 	    }
 	    
 	    // Max
 	    @GetMapping("/max")
-	    public String getMax( 
+	    public String getMax( Model model ,
 	    		         @RequestParam(value="firstValue", defaultValue="10") String firstValue 
 	                     ,@RequestParam(value="secondValue", defaultValue="10") String secondValue
 	    		) {
@@ -157,13 +164,17 @@ public class AlgorithmController {
 			try {
 				max = new Max( firstVal, secondVal);
 				result=("Result is " +  max.getResult() ) ;
+				
+				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				result= "Error: Incorrect usage";
+				//result= "Error: Incorrect usage";
+				model.addAttribute("Usage", "Error: Incorrect usage");
+				return ("viewUsage");
 			}
-			
-			return ( result );
+			model.addAttribute("Result",  result);
+			return ( "viewResult" ); //View: result.html
 	    }
 	    
 	   
